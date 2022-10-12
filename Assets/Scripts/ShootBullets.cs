a910ada35bae42c581a28208379dfb584f0c15f3
Jennifer Ruan <jennifer.y.ruan@gmail.com>
2022-10-12 15:53:16 -0400

    fixed shooters

--- Assets/Scripts/ShootBullets.cs
+++ Assets/Scripts/ShootBullets.cs
@@ -6,42 +6,43 @@ public class ShootBullets : MonoBehaviour
 {
 
     public GameObject bullet;
-    public Transform shooter;
+    public GameObject shooter;
     public float speed;
     public GameObject target;
 
     public AudioSource randomSound;
     public AudioClip[] audioSources;
 
+    bool isActive = false;
+
     void Start()
     {
         speed = Random.Range(30, 50) * 1f;
         target = GameObject.Find("President");
         randomSound = gameObject.GetComponent<AudioSource>();
-        StartCoroutine(Fire());
     }
 
-    public IEnumerator Fire() 
+    void OnTriggerEnter(Collider collision) 
     {        
-        while (target)
-        {
+        if (collision.name == "President" && !isActive){
+            isActive = true;
 
             randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
             randomSound.Play ();
-
-            yield return new WaitForSeconds(0.5f);
             
-            //Create a new bullet
-            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
-
-            //Parent it to get a less messy workspace
-            newBullet.transform.parent = shooter;
+            Invoke ("Shoot", 0.5f);
 
             // //Add velocity to the bullet with a rigidbody
             // newBullet.GetComponent<Rigidbody>().velocity = speed * transform.forward;
             // Vector3 movement = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
-
-            yield return new WaitForSeconds(Random.Range(1, 5) * 1f);
         }
+    }
+
+    void Shoot(){
+        //Create a new bullet
+        GameObject newBullet = Instantiate(bullet, shooter.transform.position, shooter.transform.rotation) as GameObject;
+
+        //Parent it to get a less messy workspace
+        newBullet.transform.parent = shooter.transform;
     }
 }
