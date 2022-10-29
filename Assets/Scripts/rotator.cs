cfb13f6199c67f04d5c66be6bdcf66ff40b36d9f
kimberlytung <kimberly.tung@mail.utoronto.ca>
2022-10-29 00:08:26 -0400

    fixed camera to detect left and right directions

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -21,10 +21,30 @@ public class rotator : MonoBehaviour
         if (target){
         turn.x += Input.GetAxis("Mouse X");
         //turn.y += Input.GetAxis("Mouse Y");
-        AvoidCrossWall();
+        // AvoidCrossWall();
+        detect(Vector3.forward, Vector3.back);
+        detect(Vector3.back, Vector3.forward);
+        detect(Vector3.left, Vector3.right);
+        detect(Vector3.right, Vector3.left);
+        // rotate around y axis
         transform.localRotation = Quaternion.Euler(0, turn.x, 0);
         }
     }
+    
+    void detect(Vector3 dir, Vector3 inverse_dir)
+    {
+        RaycastHit hitInfo;
+        Vector3 _dir = Camera.main.transform.TransformDirection(dir);
+        if (Physics.SphereCast(Camera.main.transform.position, 0.5f, _dir, out hitInfo, 0.5f))
+        {
+            float dis = hitInfo.distance;
+            Vector3 correction = Vector3.Normalize(Camera.main.transform.TransformDirection(inverse_dir)) * dis;
+            correction.z = 0f;
+            correction.y = 0f;
+            Camera.main.transform.position += correction;
+        }
+    }
+ 
 
     private void AvoidCrossWall()
     {
