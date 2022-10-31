356bb2a3ad834c92e1f68b800c0224c236f30bd6
Frank <tianfrank@gmail.com>
2022-10-31 04:56:46 -0400

    bomb radius fix

--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -6,8 +6,8 @@ public class ExplosionScript : MonoBehaviour
 {
     List<Transform> chars = new List<Transform>();
     GameObject prez;
-    GameObject smallr;
-    GameObject bigr;
+    Transform smallr;
+    Transform bigr;
     public float lethalRad;
     public float knockRad;
 
@@ -28,11 +28,11 @@ public class ExplosionScript : MonoBehaviour
         chars.Add(prez.transform);
         // Debug.Log("added Prez");
 
-        smallr = GameObject.Find("smallrad");
-        bigr = GameObject.Find("bigrad");
+        smallr = transform.Find("smallrad");
+        bigr = transform.Find("bigrad");
 
-        smallr.transform.localScale = new Vector3(lethalRad, 0.001f, lethalRad);
-        bigr.transform.localScale = new Vector3(knockRad, 0.001f, knockRad);
+        smallr.localScale = new Vector3(lethalRad, 0.001f, lethalRad);
+        bigr.localScale = new Vector3(knockRad, 0.001f, knockRad);
     }
 
     // Update is called once per frame
