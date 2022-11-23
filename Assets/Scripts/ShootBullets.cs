7085ba5ec344e3f72ff69a5661f9818f0bfccf48
Jennifer Ruan <jennifer.y.ruan@gmail.com>
2022-11-22 21:04:35 -0500

    Changed cannons to work on cooldown

--- Assets/Scripts/ShootBullets.cs
+++ Assets/Scripts/ShootBullets.cs
@@ -10,29 +10,47 @@ public class ShootBullets : MonoBehaviour
     public float speed;
     public GameObject target;
 
+    public GameObject cooldownText;
+
     public AudioClip[] shootingSounds;
 
-    bool isActive = false;
+    float lastShot = 1000;
+    public float cooldown = 3;
+
+    public string direction = "right";
 
     void Start()
     {
         target = GameObject.Find("President");
+        cooldownText.GetComponent<TMPro.TextMeshPro>().text = "";
+    }
+
+    void Update(){
+        lastShot += Time.deltaTime;
+        if (lastShot >= cooldown){
+            cooldownText.GetComponent<TMPro.TextMeshPro>().text = "";
+        }
+        else {
+            cooldownText.GetComponent<TMPro.TextMeshPro>().text = Mathf.Round(cooldown - lastShot).ToString();
+        }
     }
 
     void OnTriggerEnter(Collider collision) 
     {        
-        if (collision.name == "President" && !isActive){
-            isActive = true;
-
-            AudioSource.PlayClipAtPoint(shootingSounds[Random.Range(0, shootingSounds.Length)], transform.position);
-            
-            Invoke("Shoot", 0.1f);
+        if (collision.gameObject.tag == "Agent" || collision.gameObject.name == "President"){
+            if (lastShot >= cooldown){
+                AudioSource.PlayClipAtPoint(shootingSounds[Random.Range(0, shootingSounds.Length)], transform.position);
+                
+                Invoke("Shoot", 0.1f);
+                lastShot = 0;
+            }
         }
     }
 
     void Shoot(){
         //Create a new bullet
-        GameObject newBullet = Instantiate(bullets[Random.Range(0, bullets.Length)], shooter.GetComponent<Collider>().transform.position, shooter.GetComponent<Collider>().transform.rotation) as GameObject;
+        GameObject newBullet = Instantiate(bullets[Random.Range(0, bullets.Length)], shooter.transform.position, shooter.transform.rotation) as GameObject;
+        newBullet.GetComponent<BulletCollision>().direction = direction;
 
         //Parent it to get a less messy workspace
         newBullet.transform.parent = shooter.transform;
